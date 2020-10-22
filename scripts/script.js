d0f2644d89ea701ec6093647ee54e5c508b1b4ad
Thijscheper <45427175+Thijscheper@users.noreply.github.com>
2020-10-22 16:18:38 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ const cartButton = document.querySelector(".shoplogo");
 function openCart() {
     const cartDiv = document.querySelector(".bag");
     cartDiv.classList.toggle("visible");
-    console.log("test11")
+    
 }
 
 cartButton.addEventListener("click", openCart);// JavaScript Document
